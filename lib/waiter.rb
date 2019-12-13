3aa8a73c3a02d334d78a0d2227335decd115aa9b
github username <github email address>
2019-12-12 22:51:20 -0500

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Waiter
+
+  attr_accessor :name, :yrs_experience
+
+  @@all = []
+
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    Meal.all.max_by {|meal| meal.tip}.customer
+  end
+  # can't do max. you can't add all that with max
 
 end
